d2ddac35d632cbe295db27a5c847fc96667372fa
Shadowbeetle <tamas.kadlecsik@gmail.com>
2018-11-07 16:42:04 +0100

    initial commit

---
+++ install-remove-download.sh
@@ -0,0 +1,19 @@
+# Usage: ./install-remove-downloads.sh USERNAME
+if [ -z $1 ]; then
+  echo Usage: sudo /install-remove-downloads.sh USERNAME
+  exit 1
+fi
+
+if [ "$1" = "-h" ]; then
+  echo Usage: sudo ./install-remove-downloads.sh USERNAME
+  exit 1
+fi
+SOURCE_DIR=$(pwd)
+sed "s/{{USER}}/$1/" remove-downloads.sh.template > remove-downloads.sh
+chmod +x remove-downloads.sh
+sed "s/{{USER}}/$1/" remove-downloads.service.template > remove-downloads.service
+cd /etc/systemd/system/
+ln -sf $SOURCE_DIR/remove-downloads.timer remove-downloads.timer
+ln -sf $SOURCE_DIR/remove-downloads.service remove-downloads.service 
+cd $SOURCE_DIR
+
