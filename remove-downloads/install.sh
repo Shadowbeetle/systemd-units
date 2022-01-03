f2a29a78a9712367b15749d57540696389707210
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-01-03 11:05:34 +0100

    add path paramter to install script

--- remove-downloads/install.sh
+++ remove-downloads/install.sh
@@ -1,17 +1,18 @@
 # Usage: ./install-remove-downloads.sh USERNAME
-if [ -z $1 ]; then
-  echo Usage: sudo /install-remove-downloads.sh USERNAME
+if [ "$1" = "-h" ]; then
+  echo Usage: sudo ./install-remove-downloads.sh USERNAME PATH-TO-SCRIPT
   exit 1
 fi
 
-if [ "$1" = "-h" ]; then
-  echo Usage: sudo ./install-remove-downloads.sh USERNAME
+if [ -z $1 ] && [ -z $2 ]; then
+  echo Usage: sudo /install-remove-downloads.sh USERNAME PATH-TO-SCRIPT
   exit 1
 fi
+
 SOURCE_DIR=$(pwd)
 sed "s/{{USER}}/$1/" remove-downloads.sh.template > remove-downloads.sh
 chmod +x remove-downloads.sh
-sed "s/{{USER}}/$1/" remove-downloads.service.template > remove-downloads.service
+sed "s|{{PATH}}|$2|" remove-downloads.service.template > remove-downloads.service
 cd /etc/systemd/system/
 ln -sf $SOURCE_DIR/remove-downloads.timer remove-downloads.timer
 ln -sf $SOURCE_DIR/remove-downloads.service remove-downloads.service 
